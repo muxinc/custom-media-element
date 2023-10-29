b0ca84aa25e36aca762be9cd193cc2621ed802b3
Wesley Luyten <me@wesleyluyten.com>
2023-10-29 12:26:14 -0500

    chore: cleanup tools

--- examples/example.js
+++ examples/example.js
@@ -1,10 +1,6 @@
 import { CustomVideoElement } from '../custom-media-element.js';
 
-class MyVideoElement extends CustomVideoElement {
-  constructor() {
-    super();
-  }
-}
+class MyVideoElement extends CustomVideoElement {}
 
 if (globalThis.customElements && !globalThis.customElements.get('my-video')) {
   globalThis.customElements.define('my-video', MyVideoElement);
